3c564e1b4b16ee5fd79c3e4c0ce55871cf51d235
sybresecho <sybers_echo@yahoo.co.id>
2018-02-02 01:41:34 +0700

    init project

---
+++ src/main/java/com/kupalatu/store/KuplatustorewebappApplication.java
@@ -0,0 +1,12 @@
+package com.kupalatu.store;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KuplatustorewebappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KuplatustorewebappApplication.class, args);
+	}
+}
