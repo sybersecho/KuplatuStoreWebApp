1282b137bc2f1c83b11a02eb9556fade40ac3ffd
sybresecho <sybers_echo@yahoo.co.id>
2018-02-12 00:45:14 +0700

    fix web application test

--- src/test/java/com/kupalatu/KuplatustorewebappApplicationTests.java
+++ src/test/java/com/kupalatu/KuplatustorewebappApplicationTests.java
@@ -1,14 +1,17 @@
 package com.kupalatu;
 
-import org.junit.Ignore;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
 
-//@RunWith(SpringRunner.class)
+@RunWith(SpringRunner.class)
 //@SpringBootTest
+@ContextConfiguration
 public class KuplatustorewebappApplicationTests {
 
 	@Test
-	@Ignore
+//	@Ignore
 	public void contextLoads() {
 	}
 
